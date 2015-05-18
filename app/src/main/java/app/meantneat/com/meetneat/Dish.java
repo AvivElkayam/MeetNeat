ac7a7f5540097187b90679e346604f7e442175f9
Dan Later <ltrdan@gmail.com>
2015-05-18 15:57:46 +0300

    //dish class + event class

---
+++ app/src/main/java/app/meantneat/com/meetneat/Dish.java
@@ -0,0 +1,22 @@
+package app.meantneat.com.meetneat;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by DanltR on 18/05/2015.
+ */
+public class Dish {
+
+    private int eventId;
+    private String name;
+    private String descriprion;
+
+    private double price;
+    private double quantity;
+    private double quantityLeft;
+    private boolean isTakeAway;
+    private boolean isToSit;
+    private Bitmap picture;
+
+
+}
---
+++ app/src/main/java/app/meantneat/com/meetneat/FoodMenuSpecifiec.java
@@ -0,0 +1,17 @@
+package app.meantneat.com.meetneat;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by DanltR on 18/05/2015.
+ */
+public class FoodMenuSpecifiec {
+
+    private int eventId;
+    ArrayList<Dish> dishes;
+    private Date startDate;
+    private Date endDate;
+
+}
