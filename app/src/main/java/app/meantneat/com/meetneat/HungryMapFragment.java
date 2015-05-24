3b38e1efcaa365d667ce3c575159b6466ecfa03a
Dan Later <ltrdan@gmail.com>
2015-05-24 15:20:03 +0300

    //Added Hungry menu list - Working

--- app/src/main/java/app/meantneat/com/meetneat/HungryMapFragment.java
+++ app/src/main/java/app/meantneat/com/meetneat/HungryMapFragment.java
@@ -10,8 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TabHost;
-import com.google.android.gms.maps.*;
-import com.google.android.gms.maps.model.*;
+//import com.google.android.gms.maps.*;
+//import com.google.android.gms.maps.model.*;
 
 
 
@@ -19,43 +19,43 @@ import com.google.android.gms.maps.model.*;
  * Created by mac on 5/17/15.
  */
 
- public class HungryMapFragment extends Fragment implements OnMapReadyCallback {
+ public class HungryMapFragment extends Fragment{ //implements OnMapReadyCallback {
     @Override
-//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        super.onSaveInstanceState(savedInstanceState);
-//        View view = inflater.inflate(R.layout.hungry_map_fragment,container,false);
-//
-//        return view;
-//    }
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        View view = inflater.inflate(R.layout.hungry_map_fragment,container,false);
+
+        return view;
+    }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
     }
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.map_activity);
-
-            MapFragment mapFragment = (MapFragment) getFragmentManager()
-                    .findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
-        }
-
-        @Override
-        public void onMapReady(GoogleMap map) {
-            LatLng sydney = new LatLng(-33.867, 151.206);
-
-            map.setMyLocationEnabled(true);
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
-
-            map.addMarker(new MarkerOptions()
-                    .title("Sydney")
-                    .snippet("The most populous city in Australia.")
-                    .position(sydney));
-        }
-    }
+//        @Override
+//        public void onCreate(Bundle savedInstanceState) {
+//
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.map_activity);
+//
+//            MapFragment mapFragment = (MapFragment) getFragmentManager()
+//                    .findFragmentById(R.id.map);
+//            mapFragment.getMapAsync(this);
+//        }
+//
+//        @Override
+//        public void onMapReady(GoogleMap map) {
+//            LatLng sydney = new LatLng(-33.867, 151.206);
+//
+//            map.setMyLocationEnabled(true);
+//            map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+//
+//            map.addMarker(new MarkerOptions()
+//                    .title("Sydney")
+//                    .snippet("The most populous city in Australia.")
+//                    .position(sydney));
+//        }
+//    }
 
 
 
