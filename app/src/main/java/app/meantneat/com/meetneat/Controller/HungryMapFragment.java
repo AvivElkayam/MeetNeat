7252813fa9ca072b4d9879099d8de23e9cefc2bf
Dan Later <ltrdan@gmail.com>
2015-06-24 01:43:27 +0300

    Location Mooving

--- app/src/main/java/app/meantneat/com/meetneat/Controller/HungryMapFragment.java
+++ app/src/main/java/app/meantneat/com/meetneat/Controller/HungryMapFragment.java
@@ -4,17 +4,27 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
 import android.support.v4.app.FragmentTabHost;
+import android.util.Log;
 import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TabHost;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -24,6 +34,10 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
+import app.meantneat.com.meetneat.EventDishes;
+import app.meantneat.com.meetneat.Model.MyModel;
 import app.meantneat.com.meetneat.R;
 //import com.google.android.gms.maps.*;
 //import com.google.android.gms.maps.model.*;
@@ -34,9 +48,14 @@ import app.meantneat.com.meetneat.R;
  * Created by mac on 5/17/15.
  */
 
- public class HungryMapFragment extends Fragment implements OnMapReadyCallback {
+ public class HungryMapFragment extends Fragment implements OnMapReadyCallback , LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
     private static View view;
+    private ArrayList<EventDishes> eventsArray = new ArrayList<>();
+    private ArrayList<LatLng> coordinatesArray = new ArrayList<>();
+    private GoogleMap googleMap;
+    private GoogleApiClient mGoogleApiClient;
+    LocationRequest mLocationRequest;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
@@ -64,11 +83,25 @@ import app.meantneat.com.meetneat.R;
         android.support.v4.app.FragmentManager fragmentManager = getChildFragmentManager();
        SupportMapFragment mapFragment = (SupportMapFragment) fragmentManager.findFragmentById(R.id.map);
        mapFragment.getMapAsync(this);
+        mGoogleApiClient.connect();
+        mLocationRequest = new LocationRequest();
+
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(getActivity())
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
 
         //Google_Map = supportMapFragment.getMap();
 //               SupportMapFragment mapFragment = (SupportMapFragment) (getActivity().getSupportFragmentManager()
@@ -76,20 +109,78 @@ import app.meantneat.com.meetneat.R;
 
     }
 
+//    public interface GetClosestChefsCallback
+//    {
+//        public void done(ArrayList<Chef> ChefsClose);
+//    }
 
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        googleMap.setMyLocationEnabled(true);
+        //googleMap.animateCamera(Cmera);
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(32.073776, 34.781890), 16));
+        //ArrayList<LatLng> coordinatesArr = getClosestCoordinatesFromServer();
+        getClosestCoordinatesFromServer();
+
+    }
+
+    private void getClosestCoordinatesFromServer() {
+        MyModel.getInstance().getModel().getChefsEventFromServer(new ChefEventDishesFragment.GetEventDishesCallback() {
+            @Override
+            public void done(ArrayList<EventDishes> eventDisheses) {
+                eventsArray = eventDisheses;
+                ArrayList<LatLng> coordinatesArrayTmp = new ArrayList<>();
+                for (EventDishes e : eventsArray) {
+                    LatLng tmp = new LatLng(e.getLatitude(), e.getLongitude());
+                    coordinatesArrayTmp.add(tmp);
+
+
+
+                }
+                coordinatesArray = coordinatesArrayTmp;
+                showClosestEvents();
+            }
+        });
+    }
+
+    private void showClosestEvents()
+    {
         Drawable dr = getResources().getDrawable(R.drawable.logo1);
         Bitmap bitmap = ((BitmapDrawable) dr).getBitmap();
 
+        for (LatLng l : coordinatesArray) {
+            googleMap.addMarker(new MarkerOptions()
+                            .position(l)
+                            .title("Marker")
+                            .icon(BitmapDescriptorFactory.fromBitmap(Bitmap.createScaledBitmap(bitmap, 150, 150, true)))
+            );
+        }
+    }
 
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(32.073776, 34.781890))
-                .title("Marker")
-                .icon(BitmapDescriptorFactory.fromBitmap(Bitmap.createScaledBitmap(bitmap, 200, 200, true)))
-        );
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.e("LNGLTD", "Connected");
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+               mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(getActivity(),"Location Updated",Toast.LENGTH_SHORT).show();
+        //googleMap.clear();
     }
 //        @Override
 //        public void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/app/meantneat/com/meetneat/Model/ParseModel.java
+++ app/src/main/java/app/meantneat/com/meetneat/Model/ParseModel.java
@@ -19,6 +19,7 @@ import app.meantneat.com.meetneat.AppConstants;
 import app.meantneat.com.meetneat.Controller.ChefEventDishesFragment;
 import app.meantneat.com.meetneat.Controller.EditEventDishesFragment;
 import app.meantneat.com.meetneat.Controller.EditEventMealsFragment;
+import app.meantneat.com.meetneat.Controller.HungryMapFragment;
 import app.meantneat.com.meetneat.Controller.SignInActivity;
 import app.meantneat.com.meetneat.Dish;
 
@@ -176,7 +177,7 @@ public class ParseModel implements MyModel.ModelInterface {
             @Override
             protected Void doInBackground(Void... params) {
                 ParseQuery<ParseObject> eventQuery = new ParseQuery<ParseObject>(AppConstants.EVENT_DISHES);
-                eventQuery.whereEqualTo(AppConstants.EVENT_DISHES_CHEF_ID,ParseUser.getCurrentUser().getObjectId());
+               // eventQuery.whereEqualTo(AppConstants.EVENT_DISHES_CHEF_ID,ParseUser.getCurrentUser().getObjectId());
                 List<ParseObject> tempEventArray;
                 try {
                      tempEventArray = eventQuery.find();
@@ -254,4 +255,60 @@ public class ParseModel implements MyModel.ModelInterface {
         }
         return dishesArray;
     }
+
+//    public void getClosestChefsRadius(final HungryMapFragment.GetEventDishesCallback callback) {
+//        new AsyncTask<Void, Void, Void>() {
+//            ArrayList<EventDishes> eventDishesArrayList = new ArrayList<>();
+//            @Override
+//            protected Void doInBackground(Void... params) {
+//                ParseQuery<ParseObject> eventQuery = new ParseQuery<ParseObject>(AppConstants.EVENT_DISHES);
+//                eventQuery.whereEqualTo(AppConstants.EVENT_DISHES_CHEF_ID,ParseUser.getCurrentUser().getObjectId());
+//                List<ParseObject> tempEventArray;
+//                try {
+//                    tempEventArray = eventQuery.find();
+//                    for(ParseObject object : tempEventArray)
+//                    {
+//                        EventDishes eventDishes = new EventDishes();
+//                        eventDishes.setEventId(object.getObjectId());
+//                        eventDishes.setTitle(object.getString(AppConstants.EVENT_DISHES_TITLE));
+//
+//                        Date startingDate = object.getDate(AppConstants.EVENT_DISHES_START_DATE);
+//                        String s = startingDate.toString();
+//                        eventDishes.setEventDay(startingDate.getDay());
+//                        eventDishes.setEventMonth(startingDate.getMonth());
+//                        eventDishes.setEventYear(startingDate.getYear());
+//                        eventDishes.setStartingHour(startingDate.getHours());
+//                        eventDishes.setStartingMinute(startingDate.getMinutes());
+//
+//                        Date endingDate = object.getDate(AppConstants.EVENT_DISHES_END_DATE);
+//                        eventDishes.setEventYear(endingDate.getYear());
+//                        eventDishes.setEventMonth(endingDate.getMonth());
+//                        eventDishes.setEventDay(endingDate.getDay());
+//                        eventDishes.setEndingHour(endingDate.getHours());
+//                        eventDishes.setEndingMinute(endingDate.getMinutes());
+//
+//                        ParseGeoPoint point = object.getParseGeoPoint(AppConstants.EVENT_DISHES_GEO_POINT);
+//                        eventDishes.setLatitude(point.getLatitude());
+//                        eventDishes.setLongitude(point.getLongitude());
+//                        eventDishes.setLocation(object.getString(AppConstants.EVENT_DISHES_LOCATION));
+//                        eventDishes.setApartmentNumber(object.getString(AppConstants.EVENT_DISHES_APARTMENT_NUMBER));
+//                        //finished the event, now all the event's dishes =\
+//                        eventDishes.setEventsDishes(getDishesForEvent(eventDishes.getEventId()));
+//
+//                        eventDishesArrayList.add(eventDishes);
+//
+//                    }
+//                } catch (ParseException e) {
+//                    e.printStackTrace();
+//                }
+//                return null;
+//            }
+//
+//            @Override
+//            protected void onPostExecute(Void aVoid) {
+//                super.onPostExecute(aVoid);
+//                callback.done(eventDishesArrayList);
+//            }
+//        }.execute();
+//    }
 }
